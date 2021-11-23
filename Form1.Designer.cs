6d0892fe7c460866f96306b145563420abb9a0e4
Drake Lam <drakelam.devpro@gmail.com>
2021-11-23 15:03:58 +0700

    end

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace KillExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKill = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKill
+            // 
+            this.btnKill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKill.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKill.ForeColor = System.Drawing.Color.White;
+            this.btnKill.Location = new System.Drawing.Point(29, 7);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(115, 36);
+            this.btnKill.TabIndex = 0;
+            this.btnKill.Text = "Kill Explorer.exe";
+            this.btnKill.UseVisualStyleBackColor = true;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(156, 49);
+            this.Controls.Add(this.btnKill);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kill Explorer.exe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKill;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KillExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+            Process processo = new Process();
+
+            processo.StartInfo = new ProcessStartInfo("taskkill", "/F /IM explorer.exe");
+
+            processo.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            processo.Start();
+
+            processo.WaitForExit();
+        }
+    
+    }
+}
